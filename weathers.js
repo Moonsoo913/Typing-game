43c9a578d1946d1d9d6eca571d4bb4806befb7e9
Moonsoo913 <minssu913@naver.com>
2021-04-26 23:31:43 +0900

    add loadCoords

---
+++ weathers.js
@@ -0,0 +1,29 @@
+const weather = document.querySelector(".weathers")
+
+
+
+
+
+
+function askForCoords(){
+    
+}
+
+
+function loadCoords(){
+
+
+    const loadedCoord=localStorage.getItem(COORDS);
+    if(loadedCoord===null){
+        
+    }
+    else{
+
+    }
+}
+
+
+
+function init(){
+
+}
